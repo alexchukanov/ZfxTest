1236a38d26fe868d282d63b5c574d1e7f51a64ec
Alex Chukanov <alex.chukanov@gmail.com>
2022-05-05 04:37:17 +0300

    Add project files.

---
+++ ZfxTest/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZfxTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonShow_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+
+            int radius;
+            int.TryParse(tbRadius.Text, out radius);
+
+            int width;
+            int.TryParse(tbLengthByX.Text, out width);
+
+            int heigth;
+            int.TryParse(tbLengthByY.Text, out heigth);
+
+            string selectedShape = cbShapeTypes.SelectionBoxItem.ToString();
+
+            Shape shape = null;
+
+            switch (selectedShape)
+            {
+                case "Circle":
+                    shape = (new CircleInfo(radius)).Shape;
+                    break;
+
+                case "Square":
+                    tbLengthByY.Text = tbLengthByX.Text;
+                    heigth = width;
+
+                    shape = (new RectangleInfo(width, heigth)).Shape;
+                    break;
+
+                case "Rectangle":
+                    shape = (new RectangleInfo(width, heigth)).Shape;
+                    break;
+
+                case "Triangle":
+                    shape = (new TriangleInfo(width, heigth)).Shape;
+                    break;
+
+            }
+
+            shape.Stroke = Utils.GetSolidColorBrush("#ff3c2f2f");
+            shape.Fill = Utils.GetSolidColorBrush("#ffe0ffff");
+            shape.StrokeThickness = 8;
+
+            
+            //shape.SetValue(Canvas.LeftProperty, 200);
+            //shape.SetValue(Canvas.TopProperty, 100);
+
+            canvas.Children.Add(shape);
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+        }
+    }
+}
---
+++ ZfxTest/Model.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace ZfxTest
+{
+    public class ShapeInfo
+    {
+        public string Name { get; set; }
+        public Shape Shape { get; set; }
+    }
+
+    public class CircleInfo : ShapeInfo
+    {
+        public CircleInfo(int radius)
+        {
+            Shape = new Ellipse();
+
+            Shape.Width = radius * 2;
+            Shape.Height = radius * 2;
+        }
+    }
+
+    public class RectangleInfo : ShapeInfo
+    {
+        public RectangleInfo(int x, int y)
+        {
+            Polygon rectangle = new Polygon();
+            var rectPoints = new PointCollection
+            {
+                new Point(0, 0),
+                new Point(x, 0),
+                new Point(x, y),
+                new Point(0, y)
+            };
+
+            rectangle.Points = rectPoints;
+            Shape = rectangle;
+
+            Shape.Width = x;
+            Shape.Height = y;
+        }
+    }
+
+
+    public class TriangleInfo : ShapeInfo
+    {
+        public TriangleInfo(int x, int y)
+        {
+            Polygon rectangle = new Polygon();
+            var rectPoints = new PointCollection
+            {
+                new Point(0, 0),
+                new Point(x, 0),
+                new Point(0, y)
+            };
+
+            rectangle.Points = rectPoints;
+            Shape = rectangle;
+
+            Shape.Width = x;
+            Shape.Height = y;
+        }
+    }
+
+    public class Utils
+    {
+        public static SolidColorBrush GetSolidColorBrush(string hex)
+        {
+            hex = hex.Replace("#", string.Empty);
+            byte a = (byte)(Convert.ToUInt32(hex.Substring(0, 2), 16));
+            byte r = (byte)(Convert.ToUInt32(hex.Substring(2, 2), 16));
+            byte g = (byte)(Convert.ToUInt32(hex.Substring(4, 2), 16));
+            byte b = (byte)(Convert.ToUInt32(hex.Substring(6, 2), 16));
+            SolidColorBrush myBrush = new SolidColorBrush(System.Windows.Media.Color.FromArgb(a, r, g, b));
+
+            return myBrush;
+        }
+    }
+}
